2a0a51e2a1245266c9274d59e85f02a2efc7104c
Peter Carmines <petercarmines@gmail.com>
2018-06-16 11:47:13 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -6,75 +6,75 @@ import Counter from "./components/counter";
 import "./App.css";
 
 
-  class App extends Component {
+class App extends Component {
 
-    state = {
-        flags,
-        count: 0,
-        topScore: 0,
-        clicked: [],
-        url: flags.image
-      };
+  state = {
+    flags,
+    count: 0,
+    topScore: 0,
+    clicked: [],
+    url: flags.image
+  };
 
-      handleIncrement = (e, data) => {
+  handleIncrement = (e, data) => {
 
-        function shuffle(a) {
-          var j, x, i;
-          for (i = a.length - 1; i > 0; i--) {
-              j = Math.floor(Math.random() * (i + 1));
-              x = a[i];
-              a[i] = a[j];
-              a[j] = x;
-          }
-          return a;
+    function shuffle(a) {
+      var j, x, i;
+      for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
       }
-        
-        this.setState({ count: this.state.count + 1});
-        if (this.state.count >= this.state.topScore) {
-        this.setState({ topScore: this.state.topScore + 1});
-        }
-        console.log(e)
-        shuffle(flags)
-          for(var i=0; i < this.state.clicked.length; i++) {
-              if(this.state.clicked[i] === e) {
-                this.setState({ clicked: [], count: 0});
-                console.log("taken")
-                shuffle(flags)
-              }
-              else 
-              shuffle(flags)
-              console.log("not taken")
-              console.log(this.state.flags[e - 1].image)
-              console.log(flags)
-
-          }
-          this.state.clicked.push(e)
-      };
-  
-    render() {
-      return (
-        <Wrapper>
-            <Counter 
-            count = {this.state.count}
-            topScore = {this.state.topScore}
-            />
-          {this.state.flags.map(flag => (
-            <Image
-            increment={this.handleIncrement}
-              key={flag.id}
-              name={flag.name}
-              image={flag.image}
-              id={flag.id}
-            />
-          ))}
-        </Wrapper>
-      );
+      return a;
     }
+
+    this.setState({ count: this.state.count + 1 });
+    if (this.state.count >= this.state.topScore) {
+      this.setState({ topScore: this.state.topScore + 1 });
+    }
+    console.log(e)
+    shuffle(flags)
+    for (var i = 0; i < this.state.clicked.length; i++) {
+      if (this.state.clicked[i] === e) {
+        this.setState({ clicked: [], count: 0 });
+        console.log("taken")
+        shuffle(flags)
+      }
+      else
+        shuffle(flags)
+      console.log("not taken")
+      console.log(this.state.flags[e - 1].image)
+      console.log(flags)
+
+    }
+    this.state.clicked.push(e)
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Counter
+          count={this.state.count}
+          topScore={this.state.topScore}
+        />
+        {this.state.flags.map(flag => (
+          <Image
+            increment={this.handleIncrement}
+            key={flag.id}
+            name={flag.name}
+            image={flag.image}
+            id={flag.id}
+          />
+        ))}
+      </Wrapper>
+    );
   }
-  
-  
-  export default App;
+}
 
 
-  
-  
+export default App;
+
+
+
+
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 
-const Counter = props => 
-<section className="card">
-    <h1>Current Score: {props.count}</h1> <h1>Top Score: {props.topScore}</h1>
-</section>
+const Counter = props =>
+    <section className="card">
+        <h1>Current Score: {props.count}</h1> <h1>Top Score: {props.topScore}</h1>
+    </section>
 
 
-  
+
 
 export default Counter;
